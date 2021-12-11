28c56bb1f450f00bec0a0ad34cca235410488f40
socrateskld <77156381+socrateskld@users.noreply.github.com>
2021-12-11 12:11:08 +0000

    Add classnames in Card , fixing padding and the line

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       >
         <img src={logo} className="App-logo" alt="logo" />
         <Cards />
+        <Cards />
       </header>
     </div>
   );
--- src/Cards.js
+++ src/Cards.js
@@ -1,16 +1,15 @@
 function Cards() {
   return (
-    <div>
+    <div className="Cards">
       <h1 style={{ color: "IndianRed" }}>Radio Schedule</h1>
       <p style={{ color: "IndianRed" }}>23/12 Monday</p>
-      <p style={{ color: "Black" }}>
-        --------------------------------------------------
-      </p>
+      <hr></hr>
+
       <h2 style={{ color: "IndianRed" }}>Jazz & Funk</h2>
       <h3 style={{ color: "IndianRed	" }}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac leo ut
         velit maximus cursus. Aenean vel mauris sed arcu aliquet bibendum. In at
-        lectus eget urna malesuada posuere sit amet eget turpis. Phasellus eu
+        lectus eget urna mßalesuada posuere sit amet eget turpis. Phasellus eu
         nibh nibh. Mauris dignissim turpis lectus, ac pulvinar felis luctus a.
         Nunc commodo at odio sed molestie. Vestibulum ante ipsum primis in
         faucibus orci luctus et ultrices posuere cubilia curae; Integer varius,
@@ -18,7 +17,9 @@ function Cards() {
         eros at tortor. Etiam hendrerit neque a turpis ullamcorper, id bibendum
         ligula gravida.
       </h3>
-      <button type="button">Read More!</button>
+      <button className="Card-button" type="button">
+        Read More!
+      </button>
     </div>
   );
 }
